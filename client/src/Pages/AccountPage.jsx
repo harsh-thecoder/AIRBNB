15358ec2f76a961f9d3b95e07efc003bf5f8f934
harsh-thecoder <harshbpandey5@gmail.com>
2024-10-12 15:37:53 +0530

    Account Page Profile Accomodations and Booking

--- client/src/Pages/AccountPage.jsx
+++ client/src/Pages/AccountPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../Components/UserContext'
-import { Navigate } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function AccountPage() {
     const {ready,user} = useContext(UserContext);
@@ -8,13 +9,28 @@ function AccountPage() {
         return "Loading......"
     }
 
-    if(!user && !ready){
+    if(!user && ready){
         <Navigate to = {'/login'}/>
     }
 
+   const {subpage} = useParams();
+   
+   function linkClasses(type = null){
+       let classes = 'py-2 px-4';
+       if(type === subpage || subpage === undefined && type === 'profile'){
+        classes += ' bg-primary text-white rounded-full';
+       }
+     return classes;
+   }
+   // ' bg-primary text-white rounded-full'
+
   return (
     <div>
-        My user is {user.name}
+        <nav className='mt-8 flex justify-center gap-2'>
+            <Link className = {linkClasses('profile')} to={'/account'}>My Profile</Link>
+            <Link className = {linkClasses('bookings')} to={'/account/bookings'}>My Bookings</Link>
+            <Link className = {linkClasses('accommodations')} to={'/account/accommodations'}>My Accommodations</Link>
+        </nav>
     </div>
   )
 }
